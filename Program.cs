a53b410e60fc4ba66818d2cccce8170fbcd6208e
Liten_PanJun <xiaopanjun233@163.com>
2021-12-12 01:02:23 +0800

    Due to the Framework version update, the automatic adjustment window of this line according to DPI is no longer effective, and the replacement will be found as soon as possible.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //因版本更新，该行依据DPI自动调整窗口的不再生效，将尽快找到替代
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
