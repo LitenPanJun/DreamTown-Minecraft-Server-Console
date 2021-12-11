07a09ae055cdb76557098f73c5fdc4fd0fd11de6
Liten_PanJun <xiaopanjun233@163.com>
2021-12-12 00:40:59 +0800

    Replace the software framework and upgrade to NET Farmworker 4.6. 2. Fine tune the window size, spacing and size of some components.

--- main.Designer.cs
+++ main.Designer.cs
@@ -61,9 +61,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             this.TabControl.Controls.Add(this.Console);
             this.TabControl.Controls.Add(this.BasicSettings);
             this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(802, 450);
+            this.TabControl.Size = new System.Drawing.Size(714, 376);
             this.TabControl.TabIndex = 0;
             // 
             // Console
@@ -74,19 +75,21 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             this.Console.Controls.Add(this.StartServer);
             this.Console.Controls.Add(this.Text_Command);
             this.Console.Controls.Add(this.OutPut);
-            this.Console.Location = new System.Drawing.Point(4, 26);
+            this.Console.Location = new System.Drawing.Point(4, 22);
+            this.Console.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Console.Name = "Console";
-            this.Console.Padding = new System.Windows.Forms.Padding(3);
-            this.Console.Size = new System.Drawing.Size(794, 420);
+            this.Console.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Console.Size = new System.Drawing.Size(706, 350);
             this.Console.TabIndex = 0;
             this.Console.Text = "控制台";
             this.Console.UseVisualStyleBackColor = true;
             // 
             // KillServer
             // 
-            this.KillServer.Location = new System.Drawing.Point(690, 273);
+            this.KillServer.Location = new System.Drawing.Point(605, 269);
+            this.KillServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.KillServer.Name = "KillServer";
-            this.KillServer.Size = new System.Drawing.Size(83, 43);
+            this.KillServer.Size = new System.Drawing.Size(93, 38);
             this.KillServer.TabIndex = 5;
             this.KillServer.Text = "强制关闭服务器";
             this.KillServer.UseVisualStyleBackColor = true;
@@ -94,9 +97,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // StopServer
             // 
-            this.StopServer.Location = new System.Drawing.Point(690, 186);
+            this.StopServer.Location = new System.Drawing.Point(605, 177);
+            this.StopServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.StopServer.Name = "StopServer";
-            this.StopServer.Size = new System.Drawing.Size(83, 43);
+            this.StopServer.Size = new System.Drawing.Size(93, 38);
             this.StopServer.TabIndex = 4;
             this.StopServer.Text = "关闭服务器";
             this.StopServer.UseVisualStyleBackColor = true;
@@ -104,9 +108,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // RestartServer
             // 
-            this.RestartServer.Location = new System.Drawing.Point(690, 104);
+            this.RestartServer.Location = new System.Drawing.Point(605, 88);
+            this.RestartServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.RestartServer.Name = "RestartServer";
-            this.RestartServer.Size = new System.Drawing.Size(83, 43);
+            this.RestartServer.Size = new System.Drawing.Size(93, 38);
             this.RestartServer.TabIndex = 3;
             this.RestartServer.Text = "重启服务器";
             this.RestartServer.UseVisualStyleBackColor = true;
@@ -114,9 +119,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // StartServer
             // 
-            this.StartServer.Location = new System.Drawing.Point(690, 18);
+            this.StartServer.Location = new System.Drawing.Point(605, 6);
+            this.StartServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.StartServer.Name = "StartServer";
-            this.StartServer.Size = new System.Drawing.Size(83, 43);
+            this.StartServer.Size = new System.Drawing.Size(93, 39);
             this.StartServer.TabIndex = 2;
             this.StartServer.Text = "启动服务器";
             this.StartServer.UseVisualStyleBackColor = true;
@@ -125,9 +131,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // Text_Command
             // 
             this.Text_Command.ForeColor = System.Drawing.Color.Gray;
-            this.Text_Command.Location = new System.Drawing.Point(8, 389);
+            this.Text_Command.Location = new System.Drawing.Point(8, 321);
+            this.Text_Command.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Text_Command.Name = "Text_Command";
-            this.Text_Command.Size = new System.Drawing.Size(659, 23);
+            this.Text_Command.Size = new System.Drawing.Size(591, 21);
             this.Text_Command.TabIndex = 1;
             this.Text_Command.Text = "输入指令并回车发送至服务端（无需加“/”）";
             this.Text_Command.Enter += new System.EventHandler(this.Commands_Enter);
@@ -136,12 +143,13 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // OutPut
             // 
-            this.OutPut.Location = new System.Drawing.Point(8, 9);
+            this.OutPut.Location = new System.Drawing.Point(7, 6);
+            this.OutPut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.OutPut.Multiline = true;
             this.OutPut.Name = "OutPut";
             this.OutPut.ReadOnly = true;
             this.OutPut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.OutPut.Size = new System.Drawing.Size(659, 374);
+            this.OutPut.Size = new System.Drawing.Size(592, 301);
             this.OutPut.TabIndex = 0;
             this.OutPut.TabStop = false;
             this.OutPut.Text = "[Console]欢迎使用DTMCSC，现在时间：YYYY.MM.DD HH:MM";
@@ -160,19 +168,21 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             this.BasicSettings.Controls.Add(this.Browse_Java);
             this.BasicSettings.Controls.Add(this.Label_JavaPath);
             this.BasicSettings.Controls.Add(this.Text_JavaPath);
-            this.BasicSettings.Location = new System.Drawing.Point(4, 26);
+            this.BasicSettings.Location = new System.Drawing.Point(4, 22);
+            this.BasicSettings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BasicSettings.Name = "BasicSettings";
-            this.BasicSettings.Padding = new System.Windows.Forms.Padding(3);
-            this.BasicSettings.Size = new System.Drawing.Size(794, 420);
+            this.BasicSettings.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BasicSettings.Size = new System.Drawing.Size(706, 350);
             this.BasicSettings.TabIndex = 1;
             this.BasicSettings.Text = "基本设置";
             this.BasicSettings.UseVisualStyleBackColor = true;
             // 
             // NoSave_BasicSettings
             // 
-            this.NoSave_BasicSettings.Location = new System.Drawing.Point(627, 389);
+            this.NoSave_BasicSettings.Location = new System.Drawing.Point(542, 313);
+            this.NoSave_BasicSettings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NoSave_BasicSettings.Name = "NoSave_BasicSettings";
-            this.NoSave_BasicSettings.Size = new System.Drawing.Size(157, 23);
+            this.NoSave_BasicSettings.Size = new System.Drawing.Size(156, 29);
             this.NoSave_BasicSettings.TabIndex = 11;
             this.NoSave_BasicSettings.Text = "取消保存并回退所有修改";
             this.NoSave_BasicSettings.UseVisualStyleBackColor = true;
@@ -180,9 +190,10 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // Save_BasicSettings
             // 
-            this.Save_BasicSettings.Location = new System.Drawing.Point(530, 389);
+            this.Save_BasicSettings.Location = new System.Drawing.Point(460, 313);
+            this.Save_BasicSettings.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Save_BasicSettings.Name = "Save_BasicSettings";
-            this.Save_BasicSettings.Size = new System.Drawing.Size(75, 23);
+            this.Save_BasicSettings.Size = new System.Drawing.Size(76, 29);
             this.Save_BasicSettings.TabIndex = 10;
             this.Save_BasicSettings.Text = "保存设置";
             this.Save_BasicSettings.UseVisualStyleBackColor = true;
@@ -190,43 +201,46 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // 
             // Text_Xmx
             // 
-            this.Text_Xmx.Location = new System.Drawing.Point(556, 142);
+            this.Text_Xmx.Location = new System.Drawing.Point(477, 100);
+            this.Text_Xmx.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Text_Xmx.Name = "Text_Xmx";
-            this.Text_Xmx.Size = new System.Drawing.Size(70, 23);
+            this.Text_Xmx.Size = new System.Drawing.Size(61, 21);
             this.Text_Xmx.TabIndex = 9;
             this.Text_Xmx.Text = "1024";
             // 
             // Label_Xmx
             // 
             this.Label_Xmx.AutoSize = true;
-            this.Label_Xmx.Location = new System.Drawing.Point(378, 145);
+            this.Label_Xmx.Location = new System.Drawing.Point(324, 102);
             this.Label_Xmx.Name = "Label_Xmx";
-            this.Label_Xmx.Size = new System.Drawing.Size(172, 17);
+            this.Label_Xmx.Size = new System.Drawing.Size(161, 12);
             this.Label_Xmx.TabIndex = 8;
             this.Label_Xmx.Text = "服务器最小可用内存（MB）：";
             // 
             // Text_Xms
             // 
-            this.Text_Xms.Location = new System.Drawing.Point(231, 142);
+            this.Text_Xms.Location = new System.Drawing.Point(198, 100);
+            this.Text_Xms.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Text_Xms.Name = "Text_Xms";
-            this.Text_Xms.Size = new System.Drawing.Size(70, 23);
+            this.Text_Xms.Size = new System.Drawing.Size(61, 21);
             this.Text_Xms.TabIndex = 7;
             this.Text_Xms.Text = "512";
             // 
             // Label_Xms
             // 
             this.Label_Xms.AutoSize = true;
-            this.Label_Xms.Location = new System.Drawing.Point(53, 145);
+            this.Label_Xms.Location = new System.Drawing.Point(45, 102);
             this.Label_Xms.Name = "Label_Xms";
-            this.Label_Xms.Size = new System.Drawing.Size(172, 17);
+            this.Label_Xms.Size = new System.Drawing.Size(161, 12);
             this.Label_Xms.TabIndex = 6;
             this.Label_Xms.Text = "服务器最小可用内存（MB）：";
             // 
             // Browse_Core
             // 
-            this.Browse_Core.Location = new System.Drawing.Point(632, 85);
+            this.Browse_Core.Location = new System.Drawing.Point(542, 60);
+            this.Browse_Core.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Browse_Core.Name = "Browse_Core";
-            this.Browse_Core.Size = new System.Drawing.Size(103, 23);
+            this.Browse_Core.Size = new System.Drawing.Size(88, 21);
             this.Browse_Core.TabIndex = 5;
             this.Browse_Core.Text = "浏览…";
             this.Browse_Core.UseVisualStyleBackColor = true;
@@ -234,9 +248,9 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // Label_CorePath
             // 
             this.Label_CorePath.AutoSize = true;
-            this.Label_CorePath.Location = new System.Drawing.Point(53, 88);
+            this.Label_CorePath.Location = new System.Drawing.Point(45, 62);
             this.Label_CorePath.Name = "Label_CorePath";
-            this.Label_CorePath.Size = new System.Drawing.Size(68, 17);
+            this.Label_CorePath.Size = new System.Drawing.Size(65, 12);
             this.Label_CorePath.TabIndex = 4;
             this.Label_CorePath.Text = "核心路径：";
             // 
@@ -245,19 +259,21 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             this.Text_CorePath.AllowDrop = true;
             this.Text_CorePath.Enabled = false;
             this.Text_CorePath.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.Text_CorePath.Location = new System.Drawing.Point(128, 85);
+            this.Text_CorePath.Location = new System.Drawing.Point(110, 60);
+            this.Text_CorePath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Text_CorePath.Name = "Text_CorePath";
             this.Text_CorePath.ReadOnly = true;
-            this.Text_CorePath.Size = new System.Drawing.Size(498, 23);
+            this.Text_CorePath.Size = new System.Drawing.Size(427, 21);
             this.Text_CorePath.TabIndex = 3;
             this.Text_CorePath.TabStop = false;
             this.Text_CorePath.Text = "C:\\Users\\Administrator\\Dasktop\\Server\\mincraft_server.jar";
             // 
             // Browse_Java
             // 
-            this.Browse_Java.Location = new System.Drawing.Point(632, 27);
+            this.Browse_Java.Location = new System.Drawing.Point(542, 19);
+            this.Browse_Java.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Browse_Java.Name = "Browse_Java";
-            this.Browse_Java.Size = new System.Drawing.Size(103, 23);
+            this.Browse_Java.Size = new System.Drawing.Size(88, 21);
             this.Browse_Java.TabIndex = 2;
             this.Browse_Java.Text = "浏览…";
             this.Browse_Java.UseVisualStyleBackColor = true;
@@ -265,9 +281,9 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             // Label_JavaPath
             // 
             this.Label_JavaPath.AutoSize = true;
-            this.Label_JavaPath.Location = new System.Drawing.Point(53, 30);
+            this.Label_JavaPath.Location = new System.Drawing.Point(45, 21);
             this.Label_JavaPath.Name = "Label_JavaPath";
-            this.Label_JavaPath.Size = new System.Drawing.Size(69, 17);
+            this.Label_JavaPath.Size = new System.Drawing.Size(65, 12);
             this.Label_JavaPath.TabIndex = 1;
             this.Label_JavaPath.Text = "Java路径：";
             // 
@@ -276,21 +292,23 @@ namespace DTMCSC_DreamTown_Minecraft_Server_Console
             this.Text_JavaPath.AllowDrop = true;
             this.Text_JavaPath.Enabled = false;
             this.Text_JavaPath.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.Text_JavaPath.Location = new System.Drawing.Point(128, 27);
+            this.Text_JavaPath.Location = new System.Drawing.Point(110, 19);
+            this.Text_JavaPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Text_JavaPath.Name = "Text_JavaPath";
             this.Text_JavaPath.ReadOnly = true;
-            this.Text_JavaPath.Size = new System.Drawing.Size(498, 23);
+            this.Text_JavaPath.Size = new System.Drawing.Size(427, 21);
             this.Text_JavaPath.TabIndex = 0;
             this.Text_JavaPath.TabStop = false;
             this.Text_JavaPath.Text = "C:\\Program Files\\Java\\jdk-16.0.1\\bin\\java.exe";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(714, 375);
             this.Controls.Add(this.TabControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Main";
             this.Text = "DreamTown Minecraft Server Console";
             this.Load += new System.EventHandler(this.Main_Load);
